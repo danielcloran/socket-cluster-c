2e780b9d5906573525230977e6819097bd3a9cff
Daniel Cloran <Daniel@cloran.com>
2021-08-27 12:25:45 -0400

    remove print

--- lib/scclient.c
+++ lib/scclient.c
@@ -381,7 +381,7 @@ static int ws_service_callback(struct lws *wsi, enum lws_callback_reasons reason
     case LWS_CALLBACK_CLIENT_WRITEABLE: {
         if (message_queue_index != 0) {
             int publish_length = lws_write(wsi, message_queue[message_queue_index - 1] + LWS_SEND_BUFFER_PRE_PADDING, message_queue_len[message_queue_index - 1], LWS_WRITE_TEXT);
-            printf(KGRN "[Main Service] On writeable is called, sent data length: %d.\n" RESET, message_queue_len[message_queue_index - 1]);
+            // printf(KGRN "[Main Service] On writeable is called, sent data length: %d.\n" RESET, message_queue_len[message_queue_index - 1]);
             if (publish_length != -1) {
                 message_queue_index--;
 
