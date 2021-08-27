07d92fc8ed9d833ab9c2d64f4aad3ba905a7f6e5
Daniel Cloran <Daniel@cloran.com>
2021-08-27 12:19:33 -0400

    memory fixes

--- lib/scclient.c
+++ lib/scclient.c
@@ -400,7 +400,7 @@ static int ws_service_callback(struct lws *wsi, enum lws_callback_reasons reason
         }
 
         lws_callback_on_writable(wsi);
-        usleep(10000);
+        usleep(100000);
 
     } break;
     default:
