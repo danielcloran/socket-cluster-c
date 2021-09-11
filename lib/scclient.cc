c2ce4dfa6568a964fb3533c5702c535f54ed8208
Daniel Cloran <Daniel@cloran.com>
2021-09-11 14:45:18 -0400

    sleep time

--- lib/scclient.cc
+++ lib/scclient.cc
@@ -369,7 +369,7 @@ static int ws_service_callback(struct lws *wsi, enum lws_callback_reasons reason
             }
         }
         lws_callback_on_writable(wsi);
-        usleep(1000);
+        usleep(10000);
     } break;
     default:
         break;
