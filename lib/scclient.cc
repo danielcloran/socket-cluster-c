00f08bae0abc3576ea4e0cfdaa014d8da17328ee
Daniel Cloran <Daniel@cloran.com>
2021-09-12 16:47:02 -0400

    test

--- lib/scclient.cc
+++ lib/scclient.cc
@@ -382,8 +382,8 @@ static int ws_service_callback(struct lws *wsi, enum lws_callback_reasons reason
 }
 
 static void *pthread_routine(void *data) {
-    // websocket_write_back(wsi, (char *)data, -1);
-    // return NULL;
+    websocket_write_back(wsi, (char *)data, -1);
+    return NULL;
 }
 
 void _emit_int(char *event, int data) {
